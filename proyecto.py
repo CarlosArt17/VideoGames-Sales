9fe2fc5f279caa9f912e39161f5d22c725f2f1d0
Carlos Arturo <zs20006730@estudiantes.uv.mx>
2023-03-09 09:59:33 -0600

    creacion de proyecto

---
+++ proyecto.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.title('VideoGames Sales')
+st.header('Carlos Arturo Jose Fragoso - zS20006730@estudiantes.uv.mx')
+
+st.write("""
+        # Esta app sera el proyecto del parcial 1
+
+        Sera hecha por Carlos Arturo Jose Fragoso
+
+        """)
